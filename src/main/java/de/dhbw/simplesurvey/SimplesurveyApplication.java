4604af42c999d24cc728212ed97a2d33466fc07e
Patrick Braje <patrick.braje@gmail.com>
2020-09-29 12:40:37 +0200

    Initial commit

---
+++ src/main/java/de/dhbw/simplesurvey/SimplesurveyApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw.simplesurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplesurveyApplication.class, args);
+	}
+
+}
