43aa512db78f665934abea9f77f7e9cd0899acb1
Simon <simon2022@github.com>
2020-10-06 11:40:22 +0200

    fix

--- src/main/java/de/dhbw/simplesurvey/SimpleSurveyApplication.java
+++ src/main/java/de/dhbw/simplesurvey/SimpleSurveyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimplesurveyApplication {
+public class SimpleSurveyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimplesurveyApplication.class, args);
+		SpringApplication.run(SimpleSurveyApplication.class, args);
 	}
 
 }
