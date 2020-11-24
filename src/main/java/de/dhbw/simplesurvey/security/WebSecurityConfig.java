a88a07f7cfcc12661d35a309af1b0d7b59dcb5c4
PatrickB2109 <72072642+PatrickB2109@users.noreply.github.com>
2020-11-24 12:24:26 +0100

    Update WebSecurityConfig.java

--- src/main/java/de/dhbw/simplesurvey/security/WebSecurityConfig.java
+++ src/main/java/de/dhbw/simplesurvey/security/WebSecurityConfig.java
@@ -54,14 +54,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
+		http.cors().and().csrf().disable()
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
 			.anyRequest().authenticated();
-		
-		http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 }
